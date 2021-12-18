049575b7fb76f4b5db74faf80e8eceadfbdbe2b5
Harshit Pratap Singh <86252946+Harshit1047@users.noreply.github.com>
2021-12-18 08:48:30 +0530

    Add files via upload

---
+++ Frontend/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import {Provider} from 'react-redux'
+import store from './store'
+import { positions, transitions, Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic'
+// import fileUpload from 'express-fileupload';
+
+const options = {
+  timeout: 5000,
+  position: positions.TOP_RIGHT,
+  transition: transitions.SCALE
+   
+}
+
+
+ReactDOM.render(
+  <Provider store = {store}>
+    <AlertProvider template={AlertTemplate} {...options}>
+    <App />
+    </AlertProvider>
+  </Provider>,
+  document.getElementById('root')
+);
+
+
+
